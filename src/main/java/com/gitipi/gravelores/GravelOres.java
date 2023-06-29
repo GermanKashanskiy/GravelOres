a8af99163204e1c3e84ba8a8ae08f476e722459f
GermanKashanskiy <gitipim@yandex.com>
2023-06-30 01:23:26 +0300

    Initial commit

---
+++ src/main/java/com/gitipi/gravelores/GravelOres.java
@@ -0,0 +1,26 @@
+package com.gitipi.gravelores;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod("gravelores")
+public class GravelOres {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public GravelOres() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
